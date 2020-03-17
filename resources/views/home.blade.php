a83fb64725c1f7dc638a1da12666e9e3cb3cf183
victor <victor,monroy@ipswebtech.com>
2020-03-17 14:45:40 -0600

    proyecto con laravel y vue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Administra nota: "Manege Notes"</div>
+                <div class="card-header">Administra nota: "Manege Notes."</div>
 
                 <div class="card-body">
                 
